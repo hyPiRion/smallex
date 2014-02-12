93adae27eb6377c093a79135558925eb7c257c20
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-02-12 18:00:15 +0100

    Test naming consistency.

--- test/smallex/test/parser/smlx.clj
+++ test/smallex/test/parser/smlx.clj
@@ -5,6 +5,19 @@
   (:import (com.hypirion.smallex SMLXLexer)
            (clojure.lang ExceptionInfo)))
 
+(defn- filter-afterv
+  "Like filterv, but picks the item after the matched element, if existing."
+  [f coll]
+  (loop [matched []
+         [fst & rst] coll]
+    (cond (and (seq rst) (f fst))
+          (recur (conj matched (first rst))
+                 rst)
+          (seq rst)
+          (recur matched rst)
+          :else
+          matched)))
+
 (deftest test-clj-spec
   (with-open [rdr (io/reader (io/resource "clojure.smlx"))]
     (let [item-seq (iterator-seq (SMLXLexer. rdr))
@@ -14,4 +27,16 @@
                  (count (filter #(= :def (:type %)) item-seq)))))
       (testing "'alias' count in legal grammar should be equal to alias count"
         (is (= (count (:aliases grammar))
-               (count (filter #(= :alias (:type %)) item-seq))))))))
+               (count (filter #(= :alias (:type %)) item-seq)))))
+      (testing "aliases in grammar and item-seq should be equivalent"
+        (is (= (-> grammar :aliases keys set)
+               (->> item-seq
+                    (filter-afterv #(= :alias (:type %)))
+                    (map :value)
+                    set))))
+      (testing "rules in grammar and item-seq should be equivalent"
+        (is (= (-> grammar :rules keys set)
+               (->> item-seq
+                    (filter-afterv #(= :def (:type %)))
+                    (map :value)
+                    set)))))))
