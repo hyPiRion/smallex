99fa825d895bd41c6acfc39db8f897eaab2237e7
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-02-13 13:03:40 +0100

    Add alias generation.

--- test/smallex/test/generators.clj
+++ test/smallex/test/generators.clj
@@ -1,4 +1,5 @@
 (ns smallex.test.generators
+  (:refer-clojure :exclude [alias])
   (:require [simple-check.generators :as gen]
             [clojure.test :refer :all]
             [clojure.pprint :as pprint :refer [pprint]]))
@@ -25,24 +26,28 @@
 (defn use-alias [aliases]
   (gen/elements aliases))
 
-(def collection-generators [string char-set])
+(def collection-generators
+  (map #(gen/resize 20 %) [string char-set]))
 
 (declare operation)
 
 (defn- operation-generator
   [{:keys [op max-args type-fn such-that]
     :or {max-args 4 such-that (constantly true)}}]
-  (gen/sized
-   (fn [size]
-     (let [smaller-ops (gen/resize (dec size) operation)
-           arg-gen (cond-> (map #(gen/resize 20 %) collection-generators)
-                           (pos? size) (conj smaller-ops))]
-       (->> (gen/vector (gen/one-of arg-gen) 1 max-args)
-            (gen/fmap
-             (fn [args]
-               (with-meta {:type :op, :value op, :args args}
-                 {:result (type-fn (map #(-> % meta :result) args))})))
-            (gen/such-that such-that))))))
+  (fn [alias-gen]
+    (gen/sized
+     (fn [size]
+       (let [smaller-ops (gen/resize (dec size) (operation alias-gen))
+             arg-gen (cond-> (conj collection-generators alias-gen)
+                             (pos? size)
+                             ;; v- hack to get more nested ops
+                             (conj smaller-ops smaller-ops smaller-ops))]
+         (->> (gen/vector (gen/one-of arg-gen) 1 max-args)
+              (gen/fmap
+               (fn [args]
+                 (with-meta {:type :op, :value op, :args args}
+                   {:result (type-fn (map #(-> % meta :result) args))})))
+              (gen/such-that such-that)))))))
 
 (def or-op
   (operation-generator
@@ -80,10 +85,18 @@
   [or-op cat-op star-op plus-op opt-op not-op])
 
 (def operation
-  (gen/sized
-   (fn [size]
-     (let [op-generators (map #(gen/resize size %) all-op-generators)]
-       (gen/one-of op-generators)))))
+  (fn [alias-gen]
+    (gen/sized
+     (fn [size]
+       (let [op-generators (map #(gen/resize size (% alias-gen))
+                                all-op-generators)]
+         (gen/one-of op-generators))))))
+
+(defn expression
+  [alias-gen]
+  (gen/frequency [[8 (operation alias-gen)]
+                  [4 (gen/one-of collection-generators)]
+                  [1 alias-gen]]))
 
 (defn ppm [obj]
   (let [orig-dispatch pprint/*print-pprint-dispatch*]
@@ -96,5 +109,31 @@
         (orig-dispatch o))
       (pprint obj))))
 
-(deftest ^:examples print-op-result
-  (ppm (gen/sample operation)))
+(defn alias
+  [prev-aliases]
+  (let [alias-gen (if (seq prev-aliases)
+                    (gen/elements prev-aliases)
+                    string) ;; avoid throwing when empty
+        alias-name (gen/such-that #(and (seq %)
+                                        (Character/isLetter (first %))
+                                        (not-any? (fn [e] (= % (:value e)))
+                                                  prev-aliases))
+                                  (gen/resize 20 gen/string-alpha-numeric))
+        expr (expression alias-gen)]
+    (->> (gen/tuple alias-name expr)
+         (gen/fmap (fn [[alias-name expr]]
+                     {(with-meta {:type :symbol :value alias-name}
+                        {:result (-> expr meta :result)})
+                      expr})))))
+
+(defn aliases
+  [size]
+  (if (zero? size)
+    (alias nil)
+    (gen/bind (gen/resize (dec size)
+                          (gen/sized aliases))
+              (fn [alias-map]
+                (gen/fmap
+                 (fn [generated-alias]
+                   (merge alias-map generated-alias))
+                 (alias (-> alias-map keys)))))))
