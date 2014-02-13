2258abfb6b1a5412510157d0c419bc392440789d
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-02-13 21:49:52 +0100

    Begin generative testing on lexer/parser props.

--- test/smallex/test/parser/smlx.clj
+++ test/smallex/test/parser/smlx.clj
@@ -1,9 +1,14 @@
 (ns smallex.test.parser.smlx
   (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
-            [smallex.parser.smlx :as smlx])
+            [simple-check.clojure-test :as ct :refer [defspec]]
+            [simple-check.generators :as gen]
+            [simple-check.properties :as prop]
+            [smallex.parser.smlx :as smlx]
+            [smallex.test.generators :as s-gen])
   (:import (com.hypirion.smallex SMLXLexer)
-           (clojure.lang ExceptionInfo)))
+           (clojure.lang ExceptionInfo)
+           (java.io StringReader)))
 
 (defn- filter-afterv
   "Like filterv, but picks the item after the matched element, if existing."
@@ -18,13 +23,31 @@
           :else
           matched)))
 
+(defmacro ^:private change-max-size
+  [name max-size]
+  `(alter-meta! (var ~name) assoc
+                :test (fn [] (#'ct/assert-check
+                             (assoc (~name ct/*default-test-count*
+                                           :max-size ~max-size)
+                               :test-var (str '~name))))))
+
+(defspec def-count-equal-to-rule-count
+  (prop/for-all [grammar (gen/fmap str s-gen/grammar)]
+    (let [item-seq (-> grammar StringReader. SMLXLexer. iterator-seq)
+          parsed (smlx/parse item-seq)]
+      (testing "'def' count in legal grammar should be equal to rule count"
+        (is (= (count (:rules parsed))
+               (count (filter #(= :def (:type %)) item-seq))))))))
+
+(change-max-size def-count-equal-to-rule-count 40)
+
 (deftest test-clj-spec
   (with-open [rdr (io/reader (io/resource "clojure.smlx"))]
     (let [item-seq (iterator-seq (SMLXLexer. rdr))
           grammar (smlx/parse item-seq)]
       (testing "'def' count in legal grammar should be equal to rule count"
-          (is (= (count (:rules grammar))
-                 (count (filter #(= :def (:type %)) item-seq)))))
+        (is (= (count (:rules grammar))
+               (count (filter #(= :def (:type %)) item-seq)))))
       (testing "'alias' count in legal grammar should be equal to alias count"
         (is (= (count (:aliases grammar))
                (count (filter #(= :alias (:type %)) item-seq)))))
