4e516345e6aed24ad8415bc0f36e81a7904fe1ba
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-02-17 15:37:20 +0100

    Implement topological alias sorting.

--- src/smallex/reductions.clj
+++ src/smallex/reductions.clj
@@ -14,7 +14,7 @@
   (fnil conj #{}))
 
 (defn- transpose-graph
-  "Given a mapping from a vertex to a set of vertices, reverse the edge
+  "Given a mapping from a vertex to a set of vertices, reverses the edge
   direction."
   [graph]
   (reduce-kv
@@ -27,11 +27,41 @@
    {}
    graph))
 
-(defn check-cyclic-aliases
-  "Given a grammar, returns an error if there is a cyclic alias. Returns nil if
-  the aliases aren't cyclic."
+(defn- track-cycle
+  "Returns a vector with the elements of _some_ cycle which contains root. Root
+  will be both first and last element."
+  [graph root]
+  ;; TODO
+  )
+
+(defn- toposort-transposed
+  "Returns a topological sort of transposed graph, and throws an exception if
+  the graph is cyclic."
+  [graph]
+  (let [inv (transpose-graph graph)]
+    ;; Can, strictly speaking, do an (vec (rseq original)) instead
+    (letfn [(dfs [[stack visited] to-visit]
+              (case (get visited to-visit :not-visited)
+                :walking
+                (throw (ex-info "Found cycle."
+                                {:type :cycle
+                                 :cycle (track-cycle graph to-visit)}))
+                :completed [stack visited]
+                :not-visited
+                (let [visited (assoc visited to-visit :walking)
+                      [stack visited]
+                      (reduce dfs [stack visited] (get inv to-visit))]
+                  [(conj stack to-visit)
+                   (assoc visited to-visit :completed)])))]
+      (first
+       (reduce dfs [[] {}] (keys graph))))))
+
+(defn expand-alias-order
+  "Given a grammar, returns the order to expand aliases in definitions, which
+  ensures no alias is unexpanded. Throws an error if there is a cyclic alias."
   [grammar]
-  (let [alias-deps (into {} (for [[k v] (:aliases grammar)]
-                              [k (find-alias-deps v)]))]
-    ;; TODO: Topological sort here.
-    ))
+  (let [alias-deps (into (zipmap (keys (:aliases grammar))
+                                 (repeat #{}))
+                         (for [[k v] (:aliases grammar)]
+                           [k (find-alias-deps v)]))]
+    (toposort-transposed alias-deps)))
