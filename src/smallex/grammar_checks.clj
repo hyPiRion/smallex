a6d97fc6989c2679240c57f91b5293a45ab7cbdb
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-02-21 16:48:37 +0100

    Check that symbols refer to aliases.

--- src/smallex/grammar_checks.clj
+++ src/smallex/grammar_checks.clj
@@ -72,3 +72,29 @@
   (mapcat check-expr-arg-type
           (concat (vals (:aliases grammar))
                   (vals (:rules grammar)))))
+
+(defn- check-expr-symbol-refs
+  "Recursively checks for symbols not referencing aliases, and returns a lazy
+  seq of ExceptionInfos of culprits."
+  [g expr]
+  (cond (and (= :symbol (:type expr))
+             (not (contains? (:alises g) (:value expr))))
+        (if (contains? (:rules g) (:value expr))
+          (list (ex-info "Symbol cannot refer to a rule definition, must be an alias."
+                         {:type :symbol-ref, :expr expr}))
+          (list (ex-info "Couldn't find the definition for symbol."
+                         {:type :symbol-ref, :expr expr})))
+        ;; ^^ TODO: Damerau-Levensthein? =)
+        (and (= :op (:type expr))
+             (not (-> expr meta :alias-expansion)))
+        (mapcat #(check-expr-symbol-refs g %) (:args expr))
+        :else nil))
+
+(defn check-symbol-references
+  "Checks that all symbols refer to aliases, and creates understandable error
+  messages for those which doesn't. Returns a lazy seq of all non-alias
+  references as ExceptionInfos. Does not require the aliases to be expanded."
+  [grammar]
+  (mapcat #(check-expr-symbol-refs grammar %)
+          (concat (vals (:aliases grammar))
+                  (vals (:rules grammar)))))
